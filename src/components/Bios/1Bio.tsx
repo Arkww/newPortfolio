bcb10e16a9ce2d92f095b89ec9ef0b2fe7f98236
Mathieu <mathieu.jay2@gmail.com>
2025-03-12 22:02:11 +0100

    CV fix

--- src/components/Bios/1Bio.tsx
+++ src/components/Bios/1Bio.tsx
@@ -4,6 +4,7 @@ import Linkedin from '../../../public/assets/Logos/linkedin.png';
 import Mail from '../../../public/assets/Logos/mail.png';
 import Github from '../../../public/assets/Logos/github.png';
 import Instagram from '../../../public/assets/Logos/instagram.jpg';
+import CV from '../Bios/CVEN.pdf';
 
 const logos = [
     { src: Linkedin, link: 'https://www.linkedin.com/in/mathieu-jay/' },
@@ -28,7 +29,7 @@ const Bio: React.FC = () => {
                                 <img src={logo.src} alt="logo" className="w-8 h-6 object-contain transition-transform duration-200 transform hover:scale-110 hover:shadow-lg" />
                             </a>
                         ))}
-                        <a href="/newPortfolio/assets/CVEN.pdf" download className="flex flex-center">
+                        <a href={CV} download className="flex flex-center">
                         
                             <button
                                 className="flex items-center justify-center cursor-pointer bg-gradient-to-tr from-grey to-white border-2 border-black rounded-full transition duration-300 ease-in-out px-5 py-2.5 font-bold text-black hover:from-white hover:to-black hover:text-white"
